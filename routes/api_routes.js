2a9e462545a141c859d35811db8e6c9a04ac594d
NedaParvini <nedaparvini1@gmail.com>
2021-12-18 12:05:34 -0800

    api file works

--- routes/api_routes.js
+++ routes/api_routes.js
@@ -7,6 +7,7 @@ router.get("/notes", (req, res) => {
         if (err) {
             console.error(err);
         } else {
+            console.log(data);
             res.json(JSON.parse(data))
         }
     })
@@ -14,48 +15,55 @@ router.get("/notes", (req, res) => {
 
 router.post("/notes", (req, res) => {
     console.log(req.body);
-    fs.readFile('./db/db.json', 'utf8', (err, data) => {
-        if (err) {
-            console.error(err);
-        } else {
-            //Create variable = JSON.parse(data) so that we can use it as a JS object (array)//
-            const parsenote = JSON.parse(data);
-            // Add a new review
-            parsenote.push(newnote);
-           
-             // Add new note from req.body
-            //     * Push the object  from req.body to the array
+    console.log("headrout");
+    // Destructuring assignment for the items in req.body
+  const { title, text } = req.body;
+        // Variable for the object we will save
+        const newnote = {
+            title,
+            text,
 
-            // 3. Write the new array into the db.json file
-            fs.writeFile('./db/db.json',
-          JSON.stringify(parsenote, null, 4),
-          (writeErr) =>
-            writeErr
-              ? console.error(writeErr)
-              : console.info('Successfully updated notes!'),
-        );
+        };
+        fs.readFile('./db/db.json', 'utf8', (err, data) => {
+            if (err) {
+                console.error(err);
+            } else {
+                //Create variable = JSON.parse(data) so that we can use it as a JS object (array)//
+                const parsenote = JSON.parse(data);
+                console.log(data);
+                // Add a new review
+                parsenote.push(newnote);
+
+                // Add new note from req.body
+                //     * Push the object  from req.body to the array
+
+                // 3. Write the new array into the db.json file
+                fs.writeFile('./db/db.json',
+                    JSON.stringify(parsenote, null, 4),
+                    (writeErr) =>
+                        writeErr
+                            ? console.error(writeErr)
+                            : console.info('Successfully updated notes!'),
+                )
+                res.json(newnote)
             }
+        });
+
+
+       
+
+       
+
+
+        // res.json(array)//
+        router.get('/api/notes', (req, res) => {
+            // Inform the client
+            res.json(`${req.method} request received `);
+
+            // Log our request to the terminal
+            console.info(`${req.method} request received `);
+        });
+
+
     });
-    const response = {
-        status: 'success',
-        body: newnote,
-      };
-  
-      console.log(response);
-      res.json(response);
-   
-      res.json('Error in posting notes'),
-    
-         
-            // res.json(array)//
-            app.get('/api/notes', (req, res) => {
-                // Inform the client
-                res.json(`${req.method} request received `);
-              
-                // Log our request to the terminal
-                console.info(`${req.method} request received `);
-            });
-                
-             
-});
 module.exports = router;
